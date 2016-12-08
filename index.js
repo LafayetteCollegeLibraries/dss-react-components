3ac0f871598f3bd15e357d4e12353f4ba6f66b4d
Adam Malantonio <malantoa@lafayette.edu>
2016-12-08 17:32:28 -0500

    better Modal namespacing

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 export { default as Button } from './lib/Button'
 export { default as Input } from './lib/Input'
 export { default as Label } from './lib/Label'
+export { default as Modal } from './lib/Modal'
 export { default as TextArea } from './lib/TextArea'
 export { default as Toggle } from './lib/Toggle'
 
--- stories/Modal.jsx
+++ stories/Modal.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { storiesOf, action } from '@kadira/storybook'
-import Modal, { Header } from '../lib/Modal'
+import { Modal } from '../'
 import Button from '../lib/Button'
 
 storiesOf('Modal', module)
   .add('default Modal', () => (
     <div>
       <Modal isOpen color="warning">
-        <Header>Hey I am a Header</Header>
+        <Modal.Header>Hey I am a Header</Modal.Header>
         <h1>HEY I AM A MODAL AMA</h1>
       </Modal>
     </div>
